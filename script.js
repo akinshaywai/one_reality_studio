6f421fb418ff1abdb34bb61cdcac8459bc1395ba
freemile2ru <sofiyullahj@gmail.com>
2023-02-20 11:29:49 +0000

    fix modal display

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ const joinButton = document.querySelector(".join");
 const emailList = document.querySelector(".email-list");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
+const email = document.querySelector("#email");
 
 joinButton.addEventListener("click", () => {
-  emailList.style.visibility = "visible";
-  emailList.style.zIndex = "999";
-  modal.classList.add("transition");
-});
+  if (email.value !== "") {
+    emailList.style.visibility = "visible";
+    emailList.style.zIndex = "999";
+    modal.classList.add("transition");
 
-close.addEventListener("click", () => {
-  emailList.style.visibility = "hidden";
-  emailList.style.zIndex = "-1";
-  modal.classList.remove("transition");
+    close.addEventListener("click", () => {
+      emailList.style.visibility = "hidden";
+      emailList.style.zIndex = "-1";
+      modal.classList.remove("transition");
+    });
+  }
 });
